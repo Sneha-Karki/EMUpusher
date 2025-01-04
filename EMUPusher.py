ffc9c3e0a46e3d061be399018cde37645e1ab9d0
Sneha Karki <sneha.karki@my.utsa.edu>
2025-01-04 14:25:56 -0600

    EMUPusher.py

--- EMUPusher.py
+++ EMUPusher.py
@@ -1,9 +1,3 @@
-# Star Pusher (a Sokoban clone)
-# By Al Sweigart al@inventwithpython.com
-# http://inventwithpython.com/pygame
-# Released under a "Simplified BSD" license
-# Pygame chapter 9 
-
 import random, sys, copy, os, pygame
 from pygame.locals import *
 from pygame import mixer
